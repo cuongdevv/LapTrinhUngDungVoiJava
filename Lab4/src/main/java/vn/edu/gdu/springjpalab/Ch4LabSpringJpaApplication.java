ec3372588d8b948c0cc00ad595b2163e5e19d0a4
cuong <taihuu299@gmail.com>
2026-07-17 08:50:41 +0700

    chore: remove javadoc comment from Lab4 main class

--- Lab4/src/main/java/vn/edu/gdu/springjpalab/Ch4LabSpringJpaApplication.java
+++ Lab4/src/main/java/vn/edu/gdu/springjpalab/Ch4LabSpringJpaApplication.java
@@ -3,9 +3,6 @@ package vn.edu.gdu.springjpalab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Điểm khởi động ứng dụng Spring Boot cho Lab 4 - Spring Data JPA.
- */
 @SpringBootApplication
 public class Ch4LabSpringJpaApplication {
 
